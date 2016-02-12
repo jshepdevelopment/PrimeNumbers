ae93cc8a13016e4b6b61ae671f75b1c1b48da9fa
Mac <mac@macbook.local>
2016-02-11 19:38:33 -0700

    Initial Commit

---
+++ Prime Numbers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prime Numbers
+//
+//  Created by Mac on 2/11/16.
+//  Copyright © 2016 Salt Lake Community College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

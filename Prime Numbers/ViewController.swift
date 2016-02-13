120b62881380cf855b5fbc4d858a964fe67604f5
Mac <mac@macbook.local>
2016-02-12 19:56:48 -0700

    updated

--- Prime Numbers/ViewController.swift
+++ Prime Numbers/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var max = 100
+    var isPrime = true
+    var primeArray = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for var i = 2; i <= max; i++ {
+            
+            //reset isPrime flag
+            isPrime = true;
+            
+            for (index, number) in primeArray.enumerate() {
+                
+                if i % number.integerValue == 0 {
+                    isPrime = false
+                }
+                
+                print("Found \(number) at position \(index)")
+                print("Loop counter: \(i-2)")
+            }
+            
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {

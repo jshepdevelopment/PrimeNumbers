d8a2e0244445e8e5540da478beee172ab089e27c
Mac <mac@macbook.local>
2016-02-13 09:37:23 -0700

    updated

--- Prime Numbers/ViewController.swift
+++ Prime Numbers/ViewController.swift
@@ -13,20 +13,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var numberSlider: UISlider!
     @IBOutlet weak var counterLabel: UILabel!
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var labelCover: UILabel!
     
     @IBAction func sliderValueChanged(sender: UISlider) {
         let currentValue = Int(sender.value)
         counterLabel.text = "\(currentValue)"
     }
     
-    var max = 100
+    var max = 1000
     var isPrime = true
     var primeArray: [Int] = []
     var buttonY = 0
     var buttonX = 0
+    var timerCounter = 0
+    var animSpeed = 10
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //set up timer
+        var timer = NSTimer.scheduledTimerWithTimeInterval(0.03, target: self, selector: "update", userInfo: nil, repeats: true)
         
         // Do any additional setup after loading the view, typically from a nib.
         
@@ -49,7 +55,7 @@ class ViewController: UIViewController {
             
             if(isPrime) {
                 primeArray.append(i)
-                print("\(i) added to array.")
+                //print("\(i) added to array.")
             }
             
             let numberButton = UIButton(frame: CGRect(x: buttonX, y: buttonY, width: 50, height: 15))
@@ -58,7 +64,7 @@ class ViewController: UIViewController {
             
             
             numberButton.layer.cornerRadius = 5  // get some fancy pantsy rounding
-            numberButton.backgroundColor = UIColor.darkGrayColor()
+            numberButton.backgroundColor = UIColor.lightGrayColor()
             numberButton.setTitle("\(i)", forState: UIControlState.Normal)
             numberButton.titleLabel?.text = "\(i)"
             //numberButton.addTarget(self, action: "villainButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
@@ -66,6 +72,8 @@ class ViewController: UIViewController {
             //self.view.addSubview(numberButton)
             scrollView.addSubview(numberButton)
             
+            //coordinate casting to CGFloat
+            //scrollView.setContentOffset(CGPointMake(0, CGFloat(i+i)), animated: true)
         }
         
         
@@ -75,7 +83,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func update() {
+        // Something cool
+        scrollView.setContentOffset(CGPointMake(0, CGFloat(timerCounter*animSpeed)), animated: true)
+        print("Timer: \(timerCounter)")
+        timerCounter++
 
+
+    }
 
 }
 
